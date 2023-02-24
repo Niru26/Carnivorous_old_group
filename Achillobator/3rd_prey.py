5b8180451e99d4e524be2520599101df58109840
Nikolay <nikolayrudakov@mail.ru>
2023-02-25 02:21:10 +0600

    Made some small modifications in code

--- Achillobator/3rd_prey.py
+++ Achillobator/3rd_prey.py
@@ -1,30 +1,31 @@
 # Homework 1 by N.Rudakov
 # Task 6
 
+ticket_number = int(input("Enter 6 digits number: "))
 trigger = True
 
 while trigger:
-    ticket_number = int(input("Enter 6 digits number: "))
-    if len(str(ticket_number)) < 6:
+    digit_length = len(str(ticket_number))
+    if digit_length != 6:
         ticket_number = int(input("Enter 6 digits number: "))
     else:
-        right_part = ticket_number % 1000
-        left_part = ticket_number // 1000
+        # right_part = ticket_number % 1000
+        # left_part = ticket_number // 1000
+        accamulator = 0
         right_part_sum = 0
         left_part_sum = 0
-        devider = 1
+        counter = 0
 
-        while right_part > 0:
-            right_term = right_part % 10
-            right_part //= 10
-            right_part_sum += right_term
-            left_term = left_part // (devider)
-            left_part_sum += left_term
-            print(f"LT - {left_term}, LT sum - {left_part_sum}")
-
-        print(f"RP - {right_part_sum}, LP - {left_part_sum}")
-        if right_part_sum == left_part_sum:
-            print("This is lucky number")
-        else:
-            print("Bad luck")
-    trigger = False
+        while ticket_number > 0:
+            term = ticket_number % 10
+            ticket_number //= 10
+            right_part_sum += term
+            counter += 1
+            if counter == 3:
+                accamulator = right_part_sum
+                right_part_sum = 0
+        left_part_sum = right_part_sum
+        print(f"LP - {left_part_sum}, RP - {right_part_sum}")
+        trigger = False
+                
+           
