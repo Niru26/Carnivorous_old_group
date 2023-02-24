127fae115cd8e29da2eb751fc5364121ab451509
Nikolay <nikolayrudakov@mail.ru>
2023-02-24 16:46:29 +0600

    finished work on task 1(1st_prey)

--- Achillobator/1st_prey.py
+++ Achillobator/1st_prey.py
@@ -1,4 +1,13 @@
 # Homework 1 by N.Rudakov
 # Task 2
 
-check_number = int(input("Enter number:"))
+initial_number = int(input("Enter number: "))
+numbers_sum = 0
+
+while (initial_number > 0):
+
+    term = initial_number % 10
+    initial_number //= 10
+    numbers_sum += term
+
+print(f"Result: {numbers_sum}")
