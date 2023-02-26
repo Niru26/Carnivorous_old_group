2bf9228b7216691d066853eecc7329b6656a5549
Nikolay <nikolayrudakov@mail.ru>
2023-02-26 12:39:26 +0600

    finished work on task 3 (3rd_prey)

--- Achillobator/3rd_prey.py
+++ Achillobator/3rd_prey.py
@@ -1,30 +1,48 @@
 # Homework 1 by N.Rudakov
 # Task 6
 
+ticket_number = int(input("Enter 6 digits number: "))
 trigger = True
 
 while trigger:
-    ticket_number = int(input("Enter 6 digits number: "))
-    if len(str(ticket_number)) < 6:
+    if len(str(ticket_number)) != 6:
         ticket_number = int(input("Enter 6 digits number: "))
     else:
-        right_part = ticket_number % 1000
-        left_part = ticket_number // 1000
+
         right_part_sum = 0
         left_part_sum = 0
-        devider = 1
+        total_sum = 0
+        counter = 0
 
-        while right_part > 0:
-            right_term = right_part % 10
-            right_part //= 10
-            right_part_sum += right_term
-            left_term = left_part // (devider)
-            left_part_sum += left_term
-            print(f"LT - {left_term}, LT sum - {left_part_sum}")
+        while ticket_number > 0:
+            term = ticket_number % 10
+            ticket_number = ticket_number // 10
+            total_sum += term
+            counter += 1
+            if counter == 3:
+                right_part_sum = total_sum
 
-        print(f"RP - {right_part_sum}, LP - {left_part_sum}")
+        left_part_sum = total_sum - right_part_sum
+        print(
+            f"Total sum - {total_sum}, right part sum - {right_part_sum}. left part sum - {left_part_sum}")
+
         if right_part_sum == left_part_sum:
-            print("This is lucky number")
+            print("This is lucky ticket")
         else:
             print("Bad luck")
-    trigger = False
+
+        trigger = False
+
+
+# Draft
+    # right_part = ticket_number % 1000
+    # left_part = ticket_number // 1000
+    # devider = 1
+    # right_term = right_part % 10
+    # right_part //= 10
+    # right_part_sum += right_term
+    # left_term = left_part // (devider)
+    # left_part_sum += left_term
+    # print(f"LT - {left_term}, LT sum - {left_part_sum}")
+
+    # print(f"RP - {right_part_sum}, LP - {left_part_sum}")
