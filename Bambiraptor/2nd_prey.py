111ebd9e912ed2db65900246a94510158c8b2fd6
Nikolay <nikolayrudakov@mail.ru>
2023-03-02 10:32:31 +0600

    Finished task 2 (2nd_prey)

---
+++ Bambiraptor/2nd_prey.py
@@ -0,0 +1,9 @@
+# Homework 2 by Rudakov N.
+# Task 10
+a = int(input())
+b = int(input())
+
+for i in range(a):
+    for j in range(b):
+        if a == i + j and b == i * j:
+            print(i, j, sep='   ')
